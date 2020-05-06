8c60001efce7e01957a0e1891ef505df55025d05
ncoblentz <ncoblentz@securityps.com>
2020-05-06 12:53:42 -0500

    mend
    Added a roles class and created a default admin user with an easy to guess password

--- AuthenticationChallenge/Models/AuthorizationRoles.cs
+++ AuthenticationChallenge/Models/AuthorizationRoles.cs
@@ -7,8 +7,8 @@ namespace AuthenticationChallenge.Models
 {
     public class AuthorizationRoles
     {
-        public static String Administrator = "Administrator";
-        public static String User = "User";
+        public const string Administrator = "Administrator";
+        public const string User = "User";
         public static List<string> AllRoles = new List<string> { Administrator,User};
     }
 }
